b2ecd376cf2bfb49e4c403b209d6685f195b89a0
“hieu” <labaohieu@gmail.com>
2026-03-12 10:08:35 +0700

    wip

--- addons/erp/models/__init__.py
+++ addons/erp/models/__init__.py
@@ -1,3 +1,6 @@
 from . import custom_app
+from . import ir_model
+from . import ir_model_fields
+from . import ir_ui_view
 from . import make_compute
 from . import build
---
+++ addons/erp/models/ir_model.py
@@ -0,0 +1,221 @@
+from odoo import models, fields, api
+from odoo.exceptions import ValidationError
+import uuid
+from odoo.fields import Command, Domain
+import xml.etree.ElementTree as ET
+import os
+
+class IrModel(models.Model):
+    _inherit = "ir.model"
+    def _default_field_id(self):
+        if self.env.context.get('install_mode'):
+            return []                   # no default field when importing
+        return [Command.create({'name': 'x_name', 'field_description': 'Name', 'ttype': 'char', 'copied': True})]
+
+    field_id = fields.One2many(
+        'ir.model.fields',
+        'model_id', string='Fields',
+        required=True, copy=True,
+        domain=lambda self: self._get_field_id_domain(),
+        default=_default_field_id
+    )
+    is_filter_manual = fields.Boolean(
+        string="Is Filter Manual",
+    )
+    from_app_id = fields.Many2one(
+        'erp.custom.app',
+        string='From App',
+    )
+
+    def _get_field_id_domain(self):
+        for record in self:
+            if record.is_filter_manual:
+                return [('state', '=', 'manual')]
+            else:
+                return []
+
+    server_action_count = fields.Integer(
+        compute="_compute_server_action_count"
+    )
+
+    view_count = fields.Integer(
+        string="Views",
+        compute="_compute_view_count"
+    )
+    base_automation_count = fields.Integer(
+        string="Automations",
+        compute="_compute_base_automation_count"
+    )
+    scheduled_action_count = fields.Integer(
+        string="Scheduled Actions",
+        compute="_compute_scheduled_action_count"
+    )
+    menu_count = fields.Integer(
+        string="Menus",
+        compute="_compute_menu_count"
+    )
+
+    def _compute_menu_count(self):
+        ActWindow = self.env['ir.actions.act_window']
+        Menu = self.env['ir.ui.menu']
+
+        for rec in self:
+            actions = ActWindow.search([
+                ('res_model', '=', rec.model)
+            ])
+            if actions:
+                action_names = [
+                    f"ir.actions.act_window,{a.id}" for a in actions
+                ]
+                rec.menu_count = Menu.search_count([
+                    ('action', 'in', action_names)
+                ])
+            else:
+                rec.menu_count = 0
+
+    def _compute_scheduled_action_count(self):
+        Cron = self.env['ir.cron']
+        for rec in self:
+            rec.scheduled_action_count = Cron.search_count([
+                ('model_id', '=', rec.id)
+            ])
+
+    def _compute_base_automation_count(self):
+        Automation = self.env['base.automation']
+        for rec in self:
+            rec.base_automation_count = Automation.search_count([
+                ('model_id', '=', rec.id)
+            ])
+
+    def _compute_view_count(self):
+        View = self.env['ir.ui.view']
+        for rec in self:
+            rec.view_count = View.search_count([
+                ('model', '=', rec.model)
+            ])
+
+    def _compute_server_action_count(self):
+        for rec in self:
+            rec.server_action_count = self.env['ir.actions.server'].search_count([
+                ('model_id', '=', rec.id)
+            ])
+
+
+    def action_view_server_actions(self):
+        self.ensure_one()
+        return {
+            'type': 'ir.actions.act_window',
+            'name': 'Server Actions',
+            'res_model': 'ir.actions.server',
+            'view_mode': 'list,form',
+            'domain': [('model_id', '=', self.id)],
+            'context': {'default_model_id': self.id},
+        }
+
+    def action_view_views(self):
+        self.ensure_one()
+        return {
+            'type': 'ir.actions.act_window',
+            'name': 'Views',
+            'res_model': 'ir.ui.view',
+            'view_mode': 'list,form',
+            'domain': [('model', '=', self.model)],
+            'context': {'default_model': self.model},
+        }
+
+    def action_view_base_automation(self):
+        self.ensure_one()
+        return {
+            'type': 'ir.actions.act_window',
+            'name': 'Views',
+            'res_model': 'base.automation',
+            'view_mode': 'list,form',
+            'domain': [('model_id', '=', self.id)],
+            'context': {'default_model_id': self.id},
+        }
+    def action_view_scheduled_actions(self):
+        self.ensure_one()
+        return {
+            'type': 'ir.actions.act_window',
+            'name': 'Scheduled Actions',
+            'res_model': 'ir.cron',
+            'view_mode': 'list,form',
+            'domain': [('model_id', '=', self.id)],
+            'context': {'default_model_id': self.id},
+        }
+    def action_view_menus(self):
+        self.ensure_one()
+        actions = self.env["ir.actions.act_window"].search([('res_model', '=', self.model)])
+        action_names = [f"ir.actions.act_window,{a.id}" for a in actions]
+
+        return {
+            'type': 'ir.actions.act_window',
+            'name': 'Menus',
+            'res_model': 'ir.ui.menu',
+            'view_mode': 'list,form',
+            'domain': [('action', 'in', action_names)],
+            'context': {'default_action': action_names[0] if action_names else False},
+        }
+    def get_model_str(self):
+        def create_model_str():
+            return f'''
+    def {self.model}():
+        """{self.name}"""
+        model_id = env['ir.model'].create(
+            {{
+                "name": "{self.name}",
+                "model": "{self.model}",
+                "from_app_id": custom_module_id.id if custom_module_id else False,
+                "state": "manual",
+                "transient": {self.transient},
+                "is_filter_manual": {self.is_filter_manual},
+                "is_mail_thread": True,
+                "is_mail_activity": True,
+                "is_filter_manual": True
+            }}
+        )
+        x_name = env['ir.model.fields'].search([('model_id', '=', model_id.id), ('name', '=', 'x_name')], limit=1)
+        if x_name:
+            x_name.unlink()
+'''
+
+        def create_fields_str():
+            fields_strs = ""
+            fields = sorted(
+                self.field_id,
+                key=lambda f: bool(f.related or f.depends)
+            )
+            for field in fields:
+                fields_strs += f'''
+        env['ir.model.fields'].create({{
+            "name": "{field.name}",
+            "field_description": "{field.field_description}",
+            "ttype": "{field.ttype}",
+            "help": {f"{field.help}" if field.help else 'False'},
+
+            "model_id": model_id.id,
+
+            "relation": {f'"{field.relation}"' if field.relation else 'False'},
+            "relation_field": {f'"{field.relation_field}"' if field.relation_field else 'False'},
+            "domain": "{field.domain}",
+
+            "related": {f'"{field.related}"' if field.related else 'False'},
+            "depends": {f'"{field.depends}"' if field.depends else 'False'},
+            "compute": {f'"""\n{field.compute}\n"""' if field.compute else 'False'},
+            "required": {field.required},
+            "readonly": {field.readonly},
+            "invisible": {field.invisible},
+            "store": {field.store},
+            "index": {field.index},
+            "copied": {field.copied},
+            "tracking": {field.tracking},
+        }})
+
+'''
+            return fields_strs
+
+        return f'''
+        {create_model_str()}
+        {create_fields_str()}
+    {self.model}()
+'''
