ce7b72f4a704ba25c448302ab7be58c5f6ba98ba
“hieu” <labaohieu@gmail.com>
2026-03-19 16:57:20 +0700

    wip

--- addons/erp/models/res_groups.py
+++ addons/erp/models/res_groups.py
@@ -12,3 +12,14 @@ class ResGroups(models.Model):
         required=True,
         copy=False,
     )
+
+class ResGroupsPrivilege(models.Model):
+    _inherit = "res.groups.privilege"
+
+    uuid = fields.Char(
+        string="uuid",
+        default=lambda self: f"uuid_{uuid.uuid4().hex}",
+        readonly=True,
+        required=True,
+        copy=False,
+    )
